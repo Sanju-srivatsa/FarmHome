31297786b68fd9cfa96c0a738c135cb368fd4634
Sanju Srivatsa <71341912+Sanju-srivatsa@users.noreply.github.com>
2021-10-03 19:40:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,66 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+//Automatic Slideshow
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2200); // Change image every 2 seconds
+}
+
 const btns = document.querySelectorAll('.btn');
 const storeProducts = document.querySelectorAll('.store-product');
 
@@ -24,7 +87,7 @@ for (i = 0; i < btns.length; i++) {
 };
 
 // SEARCH FILTER
-const search = document.getElementById("search");
+const search = document.getElementById("search-box");
 const productName = document.querySelectorAll(".product-details h2");
 
 // A BETTER WAY TO FILTER THROUGH THE PRODUCTS
@@ -43,56 +106,3 @@ function filterProducts(e) {
         }
     })
 }
-
-//Automatic Slideshow
-var slideIndex = 0;
-showSlides();
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2200); // Change image every 2 seconds
-}
-//popup login
-document.querySelector("#show-login").addEventListener("click", function () {
-  document.querySelector(".popup").classList.add("active");
-});
-document
-  .querySelector(".popup .close-btn")
-  .addEventListener("click", function () {
-    document.querySelector(".popup").classList.remove("active");
-  });
-
-  //email-validation
-function validation(){
-  var form = document.getElementById("form");
-  var email = document.getElementById("email").value;
-  var text = document.getElementById("text");
-  var pattern = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
-  if  (email.match(pattern)){
-    form.classList.add("valid");
-    form.classList.remove("invalid");
-    text.innerHTML = "Your Email Address is Valid.";
-    text.style.color = "#369c17";
-  }else{
-    form.classList.remove("valid");    form.classList.add("invalid");    text.innerHTML = "Please Enter Valid Email Address.";
-    text.style.color = "#ff0000";    
-  }
-  if  (email == ""){
-    form.classList.remove("valid");
-    form.classList.remove("invalid");
-    text.innerHTML = "Please fill this section";
-    text.style.color = "#ff0000";
-  }
-
-    }
